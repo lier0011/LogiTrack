f0197f79b70ca45d23dab1e7f8ecbb8fd23cd1a1
Erwin Liem <erwin.liem@stp-bi.de>
2025-06-12 16:14:37 +0200

    improvement in InventoryItem.cs

--- Models/InventoryItem.cs
+++ Models/InventoryItem.cs
@@ -7,12 +7,12 @@ public class InventoryItem
     [Required]
     required public string Name { get; set; }
     [Required]
-    required public int Quantity { get; set; }
+    public int Quantity { get; set; }
     [Required]
     required public string Location { get; set; }
 
     // Foreign key for Order
-    public int OrderId { get; set; }
+    public int? OrderId { get; set; }
     // Navigation property
     public Order? Order { get; set; }
 
